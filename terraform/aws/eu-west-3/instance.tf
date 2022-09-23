4a7bee81a82d44a45acfc7334ff13d26ef750ad8
rakdos <bourelly.christophe@gmail.com>
2022-09-23 11:22:56 +0200

    Add dummy and working TF files

---
+++ terraform/aws/eu-west-3/instance.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "image-id"
+    values = ["ami-0493936afbe820b28"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_key_pair" "std" {
+  key_name = "std"
+}
+
+data "aws_security_group" "std_sg" {
+  id = "sg-0786c97402c28f62e"
+}
+
+resource "aws_instance" "dummy" {
+  ami = data.aws_ami.ubuntu.id
+
+  instance_type = "t2.micro"
+  key_name      = data.aws_key_pair.std.key_name
+
+  vpc_security_group_ids = [
+    data.aws_security_group.std_sg.id
+  ]
+}
