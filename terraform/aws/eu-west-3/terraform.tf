f7494dff8b9b410da189acf1316f0237689472f5
rakdos <bourelly.christophe@gmail.com>
2022-10-07 13:18:49 +0200

    TF: Fix variable for backend

--- terraform/aws/eu-west-3/terraform.tf
+++ terraform/aws/eu-west-3/terraform.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    region = var.aws_region
+    region = "eu-west-3"
     bucket = "perso-terraform-states"
     key    = "github.com/eu-west-3/dummy-tf.tf"
   }
