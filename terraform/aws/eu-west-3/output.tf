2c5fa8eef8a496380ed09d96df1a9c8463399718
rakdos <bourelly.christophe@gmail.com>
2022-10-17 12:29:12 +0200

    Terraform: Update output to send an Ansible inventory file

--- terraform/aws/eu-west-3/output.tf
+++ terraform/aws/eu-west-3/output.tf
@@ -1,3 +1,12 @@
-output "dummy_public_ip" {
-  value = aws_instance.dummy.public_ip
+resource "local_file" "ansible_inventory" {
+  filename             = "/tmp/inventory.inv"
+  file_permission      = "0640"
+  directory_permission = "0750"
+
+  content = templatefile(
+    "template/inventory.tmpl",
+    {
+      dummyNodes = aws_instance.dummy.*.public_ip
+    }
+  )
 }
